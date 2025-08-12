c7597ba68a7b7be7d983b5b16635d8007a433f5a
Saatvik Kundoor <221399230+saatvikKundoor@users.noreply.github.com>
2025-08-11 21:44:34 -0500

    Added basic user info output. Code can now return basic metadata about followers, following, etc of the given profile.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 username = str(input("What is your username?"))
-URL = "https://www.instagram.com/" + username
-print(URL)
+URL = "https://www.instagram.com/" + username + "/"
+r = requests.get(URL)
+s = BeautifulSoup(r.text, "html.parser")
+meta = s.find("meta", property = "og:description")
+print(meta)
