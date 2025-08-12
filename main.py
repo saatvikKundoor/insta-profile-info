500f3f889697d1398478bf3b1190cd1fb006017e
Saatvik Kundoor <221399230+saatvikKundoor@users.noreply.github.com>
2025-08-11 22:11:12 -0500

    Created sorting data function. Code now sorts through data to return following, followers, and posts.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
-username = str(input("What is your username?"))
+username = str(input("What is your username? "))
 URL = "https://www.instagram.com/" + username + "/"
 r = requests.get(URL)
 s = BeautifulSoup(r.text, "html.parser")
 meta = s.find("meta", property = "og:description")
+def sort_data(dataset):
+    data = {}
+    info = dataset.split("-")[0]
+    data['Followers'] = info.split()[0]
+    data['Following'] = info.split()[2]
+    data['Posts'] = info.split()[4]
+    return(print(data))
+    
+sort_data(meta.attrs['content'])
 print(meta)
